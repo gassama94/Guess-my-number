58fb444840a840983f0ac1feaddfb45b1ec789fc
Saikou Gassama <gassamasaikou@yahoo.com>
2023-04-25 18:43:18 +0000

    Add click  Eventlistener to check button and log the value of guess to console

--- script.js
+++ script.js
@@ -3,3 +3,7 @@
 let secretNumber = Math.trunc(Math.random() * 20 +1)
 let score = 20;
 let highScore = 0;
+
+document.querySelector('.check').addEventListener('click', function(){
+    console.log(document.querySelector('.guess').value);
+})
